20e79b4d3a2f8ac7bc06f036a3ce89f05c8e2587
AnuragHMS <anuragsahare2234@gmail.com>
2014-12-08 20:41:49 +0000

    Create main.cpp
    main.cpp - Probably will have some updates - but for now this is fine

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  
+  intialiseVariables(); // Intialise variables e.g. Money, crewmembers, supplies, provisions etc...
+  playSpeech(1); // Start the introductory speech
+  Shop firstShop(1, "RandomShopOne", 3); // arg[1] is shop size, argu[2] is shop name, args[3] is amount of items in shop
+  getCrew(); // Crew hire shop - 500 each btw, and 60 provisions a month - will work through - maybe have a getSpech as well
+  initiateGame(); //go to main game function that'll set off everything after this!
+  
+  
+  return 0;
+
+}
